19082f32d6e27f6690b9f460cc7b0dac6128e95a
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2017-05-14 16:52:56 +0200

    Updated shutdown functions

--- custom/functions.shared.zsh
+++ custom/functions.shared.zsh
@@ -1,6 +1,18 @@
 # functions for sleeping etc
-function rest() { TIME=${@:-now}; sudo shutdown -s $TIME; }
-function reboot() { TIME=${@:-now}; sudo shutdown -r $TIME; }
+function rest() { TIME=${1:-now}; sudo shutdown -s $TIME; }
+function reboot() { TIME=${1:-now}; sudo shutdown -r $TIME; }
+
+function kill() {
+    TIME=${1:-now}
+    case $OSTYPE in
+        darwin*)
+        sudo shutdown -h $TIME;
+        ;;
+        linux*)
+        sudo shutdown $TIME;
+        ;;
+    esac
+}
 
 # unlocking
 function alohomora() { sudo chmod -R ${2:-700} ${1:-./*}; }
