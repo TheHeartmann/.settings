53727a1e750b8d1e1545673ea24c840003b674b6
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2017-05-14 13:26:11 +0200

    Instantiated scripts. Preparing to delete subfolders to add them to the gitignore.

---
+++ initialize.sh
@@ -0,0 +1,10 @@
+# script to install git, zsh, and clone https://github.com/TheHeartmann/.settings.git
+# to prepare for further install
+
+# install git
+
+# install zsh
+
+# install powerlevel9k
+
+# clone repo
