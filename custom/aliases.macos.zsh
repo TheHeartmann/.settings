64d8db00d8af98055085bdcd8c708f277f404669
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-06-09 07:13:29 +0200

    Updates aliases and private settings.

--- custom/aliases.macos.zsh
+++ custom/aliases.macos.zsh
@@ -13,11 +13,8 @@ alias jbs="cd ~/JetBrainsSettings/" #for local version of JetBrainsSettings repo
 alias vi="vim" # to get the good kind of vim
 
 # Westerdals navigation
-# alias w1="cd ~/MEGA/Westerdals/Spillprogrammering/2015–2016 && ls -1d */"
 alias -g w1="~/MEGA/Westerdals/Spillprogrammering/2015–2016"
-# alias w2="cd ~/MEGA/Westerdals/Spillprogrammering/2016–2017 && ls -1d */"
 alias -g w2="~/MEGA/Westerdals/Spillprogrammering/2016–2017"
-# alias w3="cd ~/MEGA/Westerdals/Spillprogrammering/2017-2018 && ls -1d */"
 alias -g w3="~/MEGA/Westerdals/Spillprogrammering/2017-2018"
 
 # general navigation
@@ -28,5 +25,7 @@ alias @="open"
 
 alias dl="diskutil list"
 alias disks="diskutil list" #double aliased in case dl gets hijacked
+
+alias dot="/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
 
 ;; esac
